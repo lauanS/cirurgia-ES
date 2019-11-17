afd69c13e728558717382cd3910594cd5932b1fd
lauanS <lauan.s.souza@gmail.com>
2019-11-17 20:01:57 -0300

    Adicionando os teste de pacientes

--- tests/Model/PacienteTest.php
+++ tests/Model/PacienteTest.php
@@ -25,18 +25,96 @@ class PacienteTest extends TestCase
         $this->assertEquals($sql, $paciente->buscaPorCpf($cpfInvalido));
     }
 
-    public function testBuscaPorNome()
+    public function testBuscaPorCpfVazio()
     {
+        $cpfInvalido = '';
+        $paciente = new Paciente();
+        $sql = "SELECT * FROM paciente WHERE paciente.cpf LIKE '".$cpfInvalido."%'";
+        $this->assertEquals($sql, $paciente->buscaPorCpf($cpfInvalido));
+    }
 
+    public function testBuscaPorNomeUnico()
+    {
+        $nome = 'Maria Ross';
+        $paciente = new Paciente();
+        $esperado = "17862793048";
+
+        if(count($paciente->buscaPorNome($nome)) == 1)
+        {
+            $this->assertEquals($esperado, ($paciente->buscaPorNome($nome))[0]->getCpf());
+        }
+        else
+        {
+            $this->assertTrue(false);
+        }
+
+    }
+
+    public function testBuscaPorNomeRepetido()
+    {
+        $nome = 'Maria de Lurdes Nunes';
+        $paciente = new Paciente();
+        $esperado = ["56831207057", "91839763884"];
+        $encontrado = Array();
+        $pacientes = $paciente->buscaPorNome($nome);
+
+        foreach ($pacientes as $p)
+        {
+            array_push($encontrado, $p->getCpf());
+        }
+
+        $this->assertEquals($esperado, $encontrado);
+    }
+
+    public function testBuscaPorNomeInvalido()
+    {
+        $nome = 'Bernadete de Melo Campos';
+        $paciente = new Paciente();
+        $esperado = "SELECT * FROM paciente WHERE paciente.nome LIKE '".$nome."%'";
+
+        $encontrado = $paciente->buscaPorNome($nome);
+
+        $this->assertEmpty($encontrado);
+    }
+
+    public function testBuscaPorNomeIncompleto()
+    {
+        $nome = 'Maria';
+        $paciente = new Paciente();
+        $esperado = ["17862793048", "56831207057", "91839763884"];
+        $encontrado = Array();
+        $pacientes = $paciente->buscaPorNome($nome);
+
+        foreach ($pacientes as $p)
+        {
+            array_push($encontrado, $p->getCpf());
+        }
+
+        $this->assertEquals($esperado, $encontrado);
     }
 
     public function testBuscaTodos()
     {
+        $paciente = new Paciente();
+        $esperado = 6;
+        $encontrado = $paciente->BuscaTodos();
 
+        $this->assertEquals($esperado, count($encontrado));
     }
 
-    public function testBuscaPorId()
+    public function testBuscaTodosVerificaPaciente()
     {
+        $paciente = new Paciente();
+        $esperado = array('Anderson Luiz Thomas', 'Maria de Lurdes Nunes', 'Maria de Lurdes Nunes', 'Maria Ross',
+            'Milena Leticia Emily Freitas','Tadeu Souza de Melo');
+        $encontrado = $paciente->BuscaTodos();
 
+        $nomesEncontrados = array();
+        foreach ($encontrado as $p){
+            array_push($nomesEncontrados, $p->getNome());
+        }
+
+        $this->assertEquals($esperado, $nomesEncontrados);
     }
+
 }
