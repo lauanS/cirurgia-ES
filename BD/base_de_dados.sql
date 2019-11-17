50c0c7457a741963365845b5b06c5eeb9d9945fa
lauanS <lauan.s.souza@gmail.com>
2019-11-17 18:55:32 -0300

    Atualização das inserções no Banco de Dados

--- BD/base_de_dados.sql
+++ BD/base_de_dados.sql
@@ -6,9 +6,21 @@ INSERT INTO `medico` (`id`, `nome`, `crm`, `telefone`)
     VALUES  (NULL, 'Cleiton Costas', '1809617', '15988752215'),
             (NULL, 'Robierto Alfine', '7169081', '15974724251');
 
+INSERT INTO `medico` (`id`, `nome`, `crm`, `telefone`)
+    VALUES  (NULL, 'Kohei Horikosh', '6584112', '21996166803'),
+            (NULL, 'Henry Luan Rezende', '3599541', '15985030337');
+
 INSERT INTO `paciente` (`id`, `nome`, `cpf`, `nascimento`, `sexo`, `telefone`, `convenio`)
     VALUES  (NULL, 'Tadeu Souza de Melo', '18465420068', '1986-05-15', 'M', '13989567412', 'Bradesco'),
             (NULL, 'Maria Ross', '17862793048', '1995-02-23', 'F', '15997814881', 'Unimed');
+
+INSERT INTO `paciente` (`id`, `nome`, `cpf`, `nascimento`, `sexo`, `telefone`, `convenio`)
+    VALUES  (NULL, 'Anderson Luiz Thomas', '45649782415', '1967-06-12', 'M', '15984369914', NULL),
+            (NULL, 'Milena Letícia Emily Freitas', '49675576944', '1968-01-07', 'F', '20999961843', 'Caixa');
+
+INSERT INTO `paciente` (`id`, `nome`, `cpf`, `nascimento`, `sexo`, `telefone`, `convenio`)
+        VALUES  (NULL, 'Maria de Lurdes Nunes', '56831207057', '1972-09-15', 'F', '83991310393', 'Samaritano '),
+                (NULL, 'Maria de Lurdes Nunes', '91839763884', '1989-10-12', 'F', '69994071794', 'Unimed');
 
 INSERT INTO `agendamento` (`id_cirurgia`, `id_medico`, `id_paciente`, `data_inicio`, `data_fim`, `observacao`)
     VALUES ('1', '1', '1', '2019-12-11 11:00:00', '2019-12-11 14:00:00', 'Paciente com problemas vasculares ');
