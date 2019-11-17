b63f0bf31b51403dae0dca7d52282f241ed6b4a2
Carlos Tadeu <38169086+CarlosTadeu@users.noreply.github.com>
2019-11-17 20:16:17 -0300

    adicionado alguns imports para autocomplete

--- source/View/_theme.php
+++ source/View/_theme.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
 
     <meta charset="utf-8">
@@ -9,40 +10,49 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?= url("vendor/twbs/bootstrap/dist/css/bootstrap.min.css") ?>">
 
-    <title><?=$title; ?></title>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+    <title><?= $title; ?></title>
 </head>
+
 <body>
-
-    <nav>
-        <?php
-        if ($v->section("sidebar")):
-            echo $v->section("sidebar");
-        else:
+    <div class="container">
+        <nav class="navbar navbar-dark bg-dark">
+            <?php
+            if ($v->section("sidebar")) :
+                echo $v->section("sidebar");
+            else :
+                ?>
+                <a class="navbar-brand" title="" href="<?= url("") ?>">Home</a>
+                <a class="navbar-brand" title="" href="<?= url("agendamento") ?>">Agendamento</a>
+                <a class="navbar-brand" title="" href="<?= url("relatorio") ?>">Relatorio</a>
+                <a class="navbar-brand" title="" href="<?= url("visualizar") ?>">Visualizar</a>
+            <?php
+            endif;
             ?>
-            <a title="" href="<?= url("")?>">Home</a>
-            <a title="" href="<?= url("agendamento") ?>">Agendamento</a>
-            <a title="" href="<?= url("relatorio") ?>">Relatorio</a>
-            <a title="" href="<?= url("visualizar") ?>">Visualizar</a>
-        <?php
-        endif;
-        ?>
-
-    </nav>
+        </nav>
+    </div>
 
     <main>
-        <?= $v->section("content");?>
+        <?= $v->section("content"); ?>
     </main>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="<?= url("vendor/twbs/bootstrap/dist/js/bootstrap.min.js") ?>"></script>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
     <?= $v->section("js"); ?>
 </body>
-<footer>
-    <?= URL_BASE; ?> - Todos os Direitos Reservados
-</footer>
+
+<div class="container">
+    <footer>
+        <?= URL_BASE; ?> - Todos os Direitos Reservados
+    </footer>
+</div>
+
 </html>
