e954e41a8b0bf8a2f54fd7f5e742fc64012c2cdd
Carlos Tadeu <38169086+CarlosTadeu@users.noreply.github.com>
2019-11-17 23:22:05 -0300

    atualizacao

--- source/View/visualizar.php
+++ source/View/visualizar.php
@@ -1,55 +1,58 @@
 <?php $v->layout("_theme"); ?>
 
-<h1><?= $pageTitle ?></h1>
-<form action="<?= url("visualizar"); ?>" method="POST">
-    <label>Inicio da Cirurgia: </label> <br>
-    <input type="date" name="dataInicio" />
-    <input type="time" name="horaInicio" />
-    <br><br>
-    <label>Fim da Cirurgia: </label> <br>
-    <input type="date" name="dataFim" />
-    <input type="time" name="horaFim" />
+<div class="container">
 
-    <button class="btn btn-lg btn-primary">buscar por range de data</button>
-</form>
+    <h1><?= $pageTitle ?></h1>
+    <form action="<?= url("visualizar"); ?>" method="POST">
+        <br>
+        <label>Inicio da Cirurgia: </label> <br>
+        <input type="date" name="dataInicio" />
+        <input type="time" name="horaInicio" />
+        <br><br>
+        <label>Fim da Cirurgia: </label> <br>
+        <input type="date" name="dataFim" />
+        <input type="time" name="horaFim" />
 
-<section>
-    <div class="col-md-6">
-        <table class="table">
-            <thead>
-            <tr>
-                <th>Medico</th>
-                <th>Paciente</th>
-                <th>Cirurgia</th>
-                <th>Data de Inicio</th>
-                <th>Previsão de Término</th>
-                <th>Observação</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-                if(!empty($agendamentos)):
-                    foreach($agendamentos as $agendamento): ?>
-                        <tr>
-                            <td><?= $agendamento->getMedico()->getNome() ?> </td>
-                            <td><?= $agendamento->getPaciente()->getNome() ?> </td>
-                            <td><?= $agendamento->getCirurgia()->getNome() ?> </td>
-                            <td><?= $agendamento->getDataInicio() ?> </td>
-                            <td><?= $agendamento->getDataFim() ?> </td>
-                            <td><?= $agendamento->getDescricao() ?> </td>
-                        </tr>
+        <br><br>
+        <button class="btn btn-lg btn-primary">Buscar por Range de Data</button>
+        <br><br>
+    </form>
+
+    <section>
+        <div class="col-md-6">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Medico</th>
+                        <th>Paciente</th>
+                        <th>Cirurgia</th>
+                        <th>Data de Inicio</th>
+                        <th>Previsão de Término</th>
+                        <th>Observação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    if (!empty($agendamentos)) :
+                        foreach ($agendamentos as $agendamento) : ?>
+                            <tr>
+                                <td><?= $agendamento->getMedico()->getNome() ?> </td>
+                                <td><?= $agendamento->getPaciente()->getNome() ?> </td>
+                                <td><?= $agendamento->getCirurgia()->getNome() ?> </td>
+                                <td><?= $agendamento->getDataInicio() ?> </td>
+                                <td><?= $agendamento->getDataFim() ?> </td>
+                                <td><?= $agendamento->getDescricao() ?> </td>
+                            </tr>
                     <?php endforeach;
-                endif;
-            ?>
-            </tbody>
-        </table>
-    </div>
+                    endif;
+                    ?>
+                </tbody>
+            </table>
+        </div>
 
-</section>
-
-
+    </section>
+</div>
 
 <?php $v->start("js"); ?>
 
 <?php $v->end("js"); ?>
-
