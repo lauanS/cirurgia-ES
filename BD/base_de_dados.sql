33cfb1a3411c036d762f60666117582ee599e478
Leo Campelo <leocampelo16@gmail.com>
2019-11-18 00:01:53 -0300

    Inserindo mais dados na base de dados

--- BD/base_de_dados.sql
+++ BD/base_de_dados.sql
@@ -1,6 +1,9 @@
 INSERT INTO `cirurgia` (`id`, `nome`, `descricao`)
-    VALUES  (NULL, 'Estafilorrafia', 'Sutura da fenda palatina.'),
-            (NULL, 'Cardotomia', 'Operação de cortar a cárdia, em casos de estenose do esôfago.');
+    VALUES  (NULL, 'Estafilorrafia', 'Sutura da fenda palatina.');
+            (NULL, 'Cardotomia', 'Operação de cortar a cárdia, em casos de estenose do esôfago.'),
+            (NULL, 'Cistectomia', 'Retirada da bexiga.'),
+            (NULL, 'Gastrectomia', 'Retirada total ou parcial do estômago.'),
+            (NULL, 'Apendicectomia', 'Retirada cirúrgica do apêndice vermiforme.');
 
 INSERT INTO `medico` (`id`, `nome`, `crm`, `telefone`)
     VALUES  (NULL, 'Cleiton Costas', '1809617', '15988752215'),
@@ -23,4 +26,7 @@ INSERT INTO `paciente` (`id`, `nome`, `cpf`, `nascimento`, `sexo`, `telefone`, `
                 (NULL, 'Maria de Lurdes Nunes', '91839763884', '1989-10-12', 'F', '69994071794', 'Unimed');
 
 INSERT INTO `agendamento` (`id_cirurgia`, `id_medico`, `id_paciente`, `data_inicio`, `data_fim`, `observacao`)
-    VALUES ('1', '1', '1', '2019-12-11 11:00:00', '2019-12-11 14:00:00', 'Paciente com problemas vasculares ');
+    VALUES ('1', '1', '1', '2019-12-11 11:00:00', '2019-12-11 14:00:00', 'Paciente com problemas vasculares '),
+           ('1', '1', '2', '2019-12-12 11:00:00', '2019-12-12 14:00:00', 'Paciente com problemas vasculares '),
+           ('3', '2', '4', '2019-12-11 11:00:00', '2019-12-11 14:00:00', 'Paciente com problemas na bexiga '),
+           ('4', '1', '3', '2019-12-11 11:00:00', '2019-12-11 14:00:00', 'Paciente com problemas estomacais ');
